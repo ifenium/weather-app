8f1512abe7f45a0ad8cac6695c78a1e48203d866
Ifeoluwa Oyelakin <oyeife@gmail.com>
2023-07-29 08:20:17 +0100

    Changed View to row and lined the image classifier

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 const WEATHER_API_URL = 'https://api.agromonitoring.com/agro/1.0/weather?lat=35&lon=139&appid=eb025c7215cb416cb758a6f93b00ec8e';
 const SOIL_API_URL = 'http://api.agromonitoring.com/agro/1.0/soil?polyid=64802a49b1c3f48b495e1450&appid=eb025c7215cb416cb758a6f93b00ec8e';
 const UVI_API_URL = 'http://api.agromonitoring.com/agro/1.0/uvi?polyid=64802a49b1c3f48b495e1450&appid=eb025c7215cb416cb758a6f93b00ec8e';
+const NDVI_API_URL = 'https://samples.agromonitoring.com/agro/1.0/ndvi/history?polyid=64802a49b1c3f48b495e1450&start=1530336000&end=1534976000&appid=eb025c7215cb416cb758a6f93b00ec8e';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [soilData, setSoilData] = useState(null);
   const [uviData, setUviData] = useState(null);
+  const [ndviData, setNdviData] = useState(null);
 
   useEffect(() => {
     // Fetch weather data from the API when the component mounts
@@ -44,37 +46,71 @@ function App() {
       .catch(error => {
         console.error('Error fetching UVI data:', error);
       });
+
+    // Fetch NDVI data from the API when the component mounts
+    axios.get(NDVI_API_URL)
+      .then(response => {
+        setNdviData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching NDVI data:', error);
+      });
   }, []);
 
   return (
     <div className="App">
+      <div className='card'>
+        <h1>Plant Leaf Image Classifier</h1>
+        <a href="https://rexsimiloluwah.github.io/PLANT-DISEASE-CLASSIFIER-WEB-APP-TENSORFLOWJS/" className="card-link" target="_blank" rel="noopener noreferrer">
+          <img className='upload' src={`${process.env.PUBLIC_URL}/Vector.png`} alt="Upload"/>
+        </a>
+        <h3>Upload Image</h3>
+      </div>
       <div className="card">
-        <h1>Weather Information</h1>
+        <h1>Weather Data</h1>
         {weatherData ? (
           <div>
             <p className="weather-info">Temperature: {weatherData.main.temp.toFixed(2)}°C</p>
             <p className="weather-info">Humidity: {weatherData.main.humidity}%</p>
             <p className="weather-info">Pressure: {weatherData.main.pressure} hPa</p>
             <p className="weather-info">Weather Description: {weatherData.weather[0].description}</p>
+            <p className="uvi-info">UV Index: {uviData.uvi}</p>
           </div>
         ) : (
           <p>Loading weather data...</p>
         )}
       </div>
-
+      <div className="logo-container">
+        <img src={`${process.env.PUBLIC_URL}/Logo.png`} alt="Logo" className="logo" />
+      </div>
       <div className="card">
-        <h1>Soil Information</h1>
+        <h1>Soil data</h1>
         {soilData ? (
           <div>
-            <p className="soil-info">Temperature: {soilData.temp}°C</p>
-            <p className="soil-info">Moisture: {soilData.moisture}%</p>
-            <p className="soil-info">EC (Electrical Conductivity): {soilData.ec} dS/m</p>
+            <p className="soil-info">Temperature at the surface: {(soilData.t0-273).toFixed(2)}°C</p>
+            <p className="soil-info">Temperature at the depth of 10 cm: {(soilData.t10-273).toFixed(2)}°C</p>
+            <p className="soil-info">Moisture (m3/m3): {(soilData.moisture*100).toFixed(2)}%</p>
           </div>
         ) : (
           <p>Loading soil data...</p>
         )}
       </div>
-
+      <div className="card">
+        <h1>Historical NDVI Data</h1>
+        {ndviData ? (
+          <div>
+            {ndviData.map((data, index) => (
+              <p key={index} className="ndvi-info">
+                Date: {new Date(data.dt * 1000).toLocaleDateString()}, NDVI Value: {data.ndvi.toFixed(2)}
+              </p>
+            ))}
+          </div>
+        ) : (
+          <p>Loading historical NDVI data...</p>
+        )}
+      </div>
+      
+      {/*
       <div className="card">
         <h1>UV Index</h1>
         {uviData ? (
@@ -85,6 +121,7 @@ function App() {
           <p>Loading UV Index data...</p>
         )}
       </div>
+        */}
     </div>
   );
 }
